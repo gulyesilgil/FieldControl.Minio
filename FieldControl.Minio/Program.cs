f1a4f866c428b4e82f4936067247c307d70121f7
Gul Yesilgil <gulyesilgilgul@gmail.com>
2026-05-03 21:28:50 +0300

    feat: add Swagger configuration

--- FieldControl.Minio/Program.cs
+++ FieldControl.Minio/Program.cs
@@ -3,7 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,6 +13,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
