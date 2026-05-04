d4750b34f6c275831439456eacfa2e56911eef9e
Gul Yesilgil <gulyesilgilgul@gmail.com>
2026-05-04 15:34:57 +0300

    feat: add file storage service abstraction

---
+++ FieldControl.Minio/Interfaces/IFileStorageService.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+
+namespace FieldControl.Minio.Interfaces
+{
+    public interface IFileStorageService
+    {
+        /// <summary>
+        /// Dosyayı Object Storage'a yükler
+        /// </summary>
+        /// <param name="bucketName">Bucket adı</param>
+        /// <param name="objectName">StoredFileName (key)</param>
+        /// <param name="data">Dosya byte verisi</param>
+        /// <param name="contentType">Dosya tipi</param>
+        /// <returns>StoredFileName (key)</returns>
+        Task<string> UploadFileAsync(
+            string bucketName,
+            string objectName,
+            byte[] data,
+            string contentType);
+
+        /// <summary>
+        /// Dosyayı Object Storage'dan indirir
+        /// </summary>
+        Task<byte[]> DownloadFileAsync(
+            string bucketName,
+            string objectName);
+
+        /// <summary>
+        /// Dosyayı Object Storage'dan siler
+        /// </summary>
+        Task DeleteFileAsync(
+            string bucketName,
+            string objectName);
+    }
+}
