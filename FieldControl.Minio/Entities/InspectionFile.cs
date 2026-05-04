a796b2203cfb844744499448d6bd8f9fecf1bddc
Gul Yesilgil <gulyesilgilgul@gmail.com>
2026-05-04 11:18:42 +0300

    feat: add domain entities and inspection status enum

---
+++ FieldControl.Minio/Entities/InspectionFile.cs
@@ -0,0 +1,19 @@
+﻿namespace FieldControl.Minio.Entities
+{
+        public class InspectionFile
+        {
+            public Guid Id { get; set; }
+            public Guid InspectionId { get; set; }
+            public string FileName { get; set; }
+            public string StoredFileName { get; set; }
+            public string ContentType { get; set; }
+            public long FileSize { get; set; }
+            public string BucketName { get; set; } = default!;
+            public DateTime CreatedAt { get; set; }
+
+            public Inspection Inspection { get; set; }
+            //inspection_files, inspection'dan id foriegn key'i alir
+            //bu reference navigationdir
+        }
+    }
+}
